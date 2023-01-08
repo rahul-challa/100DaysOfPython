999820d5a792425aa147ca1e96064a5998bc6052
Rahul Challa <challaviswanadhrahul@gmail.com>
2023-01-08 07:25:43 +0530

    Day 008

---
+++ Day 008.py
@@ -0,0 +1,12 @@
+'''
+Exercise 1 - Create a Calculator Solution
+Create a calculator capable of performing addition, subtraction, multiplication and division operations on two numbers. Your program should format the output in a readable manner!
+'''
+
+a = 50
+b = 3
+
+print("The value of", a, "+", 3, "is: ", a + b)
+print("The value of", a, "-", 3, "is: ", a - b)
+print("The value of", a, "*", 3, "is: ", a * b)
+print("The value of", a, "/", 3, "is: ", a / b)
