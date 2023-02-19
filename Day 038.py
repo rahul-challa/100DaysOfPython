ec656df88d607090cc8971d7a54e1adf3640893c
Rahul Challa <challaviswanadhrahul@gmail.com>
2023-02-19 11:01:55 +0530

    Day 039.py and Day 038.py Quiz

--- Day 038.py
+++ Day 038.py
@@ -28,3 +28,12 @@ a = int(input("Enter any value between 5 and 9"))
 
 if(a<5  or a>9):
     raise  ValueError("Value should be between 5 and 9")
+
+# Quiz
+
+a = int(input("Enter any value between 5 & 9 "))
+
+if (a<5 or a>9):
+    raise ValueError("Value must be between 5 & 9")
+elif(a == "Quit"):
+    print("Done!")
